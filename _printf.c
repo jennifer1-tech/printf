8dc0a3f2779a49e36e443b1d9497afc36d5bc9dd
jennifer1-tech <jennifer.ifychukwurah@gmail.com>
2023-01-22 11:15:06 -0800

    task

--- _printf.c
+++ _printf.c
@@ -11,7 +11,6 @@ int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -65,4 +64,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-
